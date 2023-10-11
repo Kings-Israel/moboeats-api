bf4713cd44a7484929330a50d211f856c53cddd0
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-10-11 13:59:29 +0300

    Update

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 class Payment extends Model
@@ -46,12 +47,20 @@ class Payment extends Model
                 ['id' => 2,'caption' => 'Active', 'color' => 'bg-green-500'],
             ];
         }
-        
+
         if(isset($options)){
             return $options;
         }else{
             return null;
         }
+    }
+
+    /**
+     * Get the user that owns the Payment
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 
     public function order()
