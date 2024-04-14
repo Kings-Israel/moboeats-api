37b167cba21f621f1b079e5c12d20f8b1fc2d476
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2024-04-14 13:15:36 +0300

    Update

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://admin.moboeats.test:*', 'http://restaurant.moboeats.test:*')],
+    // 'allowed_origins' => [env('FRONTEND_URL', 'http://admin.moboeats.test:*', 'http://restaurant.moboeats.test:*', 'https://admin.moboeats.com', 'https://restaurant.moboeats.com', 'https://admin.moboeats.co.uk', 'https://restaurant.moboeats.co.uk', 'https://app.moboeats.co.uk', 'https://app.moboeats.com')],
 
-    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
