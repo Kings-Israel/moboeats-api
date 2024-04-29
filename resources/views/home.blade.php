e099bdc391d7b3c34bc3f34c64c3e7f84e7f0373
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2024-04-29 07:24:44 +0300

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -93,7 +93,7 @@
             </div>
         </div>
 
-        <div class="bg-white lg:h-[52rem]">
+        <div class="bg-white pb-8">
             <h1 class="text-3xl text-center my-5 pt-6 font-semibold">How it works</h1>
             <div class="flex flex-nowrap gap-4 overflow-x-auto px-4 md:px-24 4xl:px-72 no-scrollbar">
                 <div class="flex-shrink-0 my-4">
@@ -130,37 +130,36 @@
                 </div>
             </div>
 
-            <div class="px-8 mt-14 lg:px-44">
-                <div>
+            <div class="px-8 mt-14 lg:px-28 lg:flex lg:gap-10">
+                <div class="my-auto">
                     <?xml version="1.0" encoding="utf-8"?>
 
-                    <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-                    <svg class="mx-auto" width="180px" height="180px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clip-path="url(#clip0_429_11087)">
-                    <path d="M4 4.00098H20V18.001C20 19.1055 19.1046 20.001 18 20.001H6C4.89543 20.001 4 19.1055 4 18.001V4.00098Z" stroke="#292929" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <rect x="12" y="8" width="0.01" height="0.01" stroke="#292929" stroke-width="3.75" stroke-linejoin="round"/>
-                    <path d="M12 12V16" stroke="#292929" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    </g>
-                    <defs>
-                    <clipPath id="clip0_429_11087">
-                    <rect width="24" height="24" fill="white"/>
-                    </clipPath>
-                    </defs>
+                    <!-- Uploaded to: SVG Repo, www.svgrepo.com, Transformed by: SVG Repo Mixer Tools -->
+                    <svg width="80px" height="80px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+
+                        <g id="SVGRepo_iconCarrier"> <path d="M3 5.5C3 14.0604 9.93959 21 18.5 21C18.8862 21 19.2691 20.9859 19.6483 20.9581C20.0834 20.9262 20.3009 20.9103 20.499 20.7963C20.663 20.7019 20.8185 20.5345 20.9007 20.364C21 20.1582 21 19.9181 21 19.438V16.6207C21 16.2169 21 16.015 20.9335 15.842C20.8749 15.6891 20.7795 15.553 20.6559 15.4456C20.516 15.324 20.3262 15.255 19.9468 15.117L16.74 13.9509C16.2985 13.7904 16.0777 13.7101 15.8683 13.7237C15.6836 13.7357 15.5059 13.7988 15.3549 13.9058C15.1837 14.0271 15.0629 14.2285 14.8212 14.6314L14 16C11.3501 14.7999 9.2019 12.6489 8 10L9.36863 9.17882C9.77145 8.93713 9.97286 8.81628 10.0942 8.64506C10.2012 8.49408 10.2643 8.31637 10.2763 8.1317C10.2899 7.92227 10.2096 7.70153 10.0491 7.26005L8.88299 4.05321C8.745 3.67376 8.67601 3.48403 8.55442 3.3441C8.44701 3.22049 8.31089 3.12515 8.15802 3.06645C7.98496 3 7.78308 3 7.37932 3H4.56201C4.08188 3 3.84181 3 3.63598 3.09925C3.4655 3.18146 3.29814 3.33701 3.2037 3.50103C3.08968 3.69907 3.07375 3.91662 3.04189 4.35173C3.01413 4.73086 3 5.11378 3 5.5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </g>
+
                     </svg>
                 </div>
-                <h1 class="text-3xl text-center my-2 py-2 font-semibold">Have any Complaint</h1>
-                <div class="flex justify-center">
-                    {{-- <video class="h-96 rounded-lg" controls>
-                            <source src="">
-                        </video> --}}
-                    <span class="font-bold text-xl">
-                        If you encounter any issues or have feedback regarding your shopping experience, we're here to
-                        help. To make a complaint, please visit our website and navigate to the <a
-                            href="{{ route('contact-us') }}" class="text-primary-one underline">Contact Us</a>
-                        section. You can also reach out to our customer support team directly via email or phone. Your
-                        satisfaction is our priority, and we're committed to addressing your concerns promptly and
-                        effectively.
-                    </span>
+                <div>
+                    <h1 class="text-3xl my-2 py-2 font-semibold">Have any Complaint?</h1>
+                    <div class="flex justify-center">
+                        {{-- <video class="h-96 rounded-lg" controls>
+                                <source src="">
+                            </video> --}}
+                        <span class="font-bold text-xl">
+                            If you encounter any issues or have feedback regarding your shopping experience, we're here to
+                            help. To make a complaint, please visit our website and navigate to the <a
+                                href="{{ route('contact-us') }}" class="text-primary-one underline">Contact Us</a>
+                            section. You can also reach out to our customer support team directly via email or phone. Your
+                            satisfaction is our priority, and we're committed to addressing your concerns promptly and
+                            effectively.
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
