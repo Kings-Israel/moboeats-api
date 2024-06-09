e0a9cfbcae6b81746f4db0710f2d8b95e59b5dba
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2024-06-09 10:34:23 +0300

    Update

--- app/Models/User.php
+++ app/Models/User.php
@@ -120,11 +120,7 @@ class User extends Authenticatable implements LaratrustUser
     public function getImageAttribute($value)
     {
         if ($value) {
-            if (env('APP_ENV') == 'production') {
-                return 'https://moboeats.com/storage/user/avatar/'.$value;
-            } else {
-                return config('app.url').'/storage/user/avatar/'.$value;
-            }
+            return config('app.url').'/storage/user/avatar/'.$value;
         }
         return config('app.url').'/assets/user/default.png';
     }
