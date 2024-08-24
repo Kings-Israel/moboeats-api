ca75db880249db2e13a64805f98b28da2a39eb29
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2024-08-24 10:19:32 +0300

    Update

--- app/Http/Controllers/Api/V1/RestaurantController.php
+++ app/Http/Controllers/Api/V1/RestaurantController.php
@@ -393,7 +393,7 @@ class RestaurantController extends Controller
                                 ->whereHas('rider')
                                 ->where('status', 2)
                                 ->where(function($query) use ($restaurant_id) {
-                                    $assigned_riders = Order::where('rider_id', '!=', NULL)->where('status', 'On Delivery')->get()->pluck('rider_id');
+                                    $assigned_riders = Order::where('rider_id', '!=', NULL)->where('status', 'On Delivery')->pluck('rider_id');
 
                                     $query->when(count($assigned_riders) > 0, function ($query) use ($assigned_riders) {
                                         $query->whereNotIn('id', $assigned_riders);
