b00e73a81b77c65368d66f99f79e724bcf53bf9c
Kings-Israel <milimokings@gmail.com>
2026-04-19 11:02:39 +0300

    Testing deployment

--- app/Http/Controllers/Api/V1/AdminController.php
+++ app/Http/Controllers/Api/V1/AdminController.php
@@ -59,6 +59,7 @@ class AdminController extends Controller
 
     public function login(Request $request)
     {
+        info($request->all());
         try {
             $request->validate([
                 'email' => ['required', 'string', 'email'],
